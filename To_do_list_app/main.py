abd6ee40aef5d6e16a6bf36d40a928d16725e98c
aaditya2291 <ayyala.aditya.2291@gmail.com>
2024-12-29 22:25:17 +0530

    Day 2 done

---
+++ To_do_list_app/main.py
@@ -0,0 +1,21 @@
+"""to-do-app"""
+
+Message = "Enter a todo:"
+# todo1 = input(Message)
+# print(todo1)
+# todo2 = input(Message)
+# print(todo2)
+# todo3 = input(Message)
+# print(todo3)
+
+# todos = [todo1, todo2, todo3]
+# print(todos, "\n")
+
+todo_list = []
+
+while True:
+    todo3 = input(Message)
+    todo_list.append(todo3)
+    print(todo3, "\n")
+    print("Next....", "\n")
+    print(todo_list, "\n")
