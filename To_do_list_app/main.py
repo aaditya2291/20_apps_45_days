c95e515960d0ae059615883f03da1efcf7c27420
aaditya2291 <ayyala.aditya.2291@gmail.com>
2024-12-31 12:24:44 +0530

    Day 7 wip

--- To_do_list_app/main.py
+++ To_do_list_app/main.py
@@ -134,8 +134,71 @@
 #             )
 
 
+# # -------------------------------------------------------------------------------#
+# # Day 6
+# # -------------------------------------------------------------------------------#
+
+
+# while True:
+
+#     user_action = input("type add or show or edit or complete or exit ")
+#     user_action = user_action.strip()
+#     match user_action:
+#         case "add":
+#             todo = input("enter todo: ") + "\n"
+#             file = open(
+#                 "F:\\aditya_git\\20_apps_45_days\\To_do_list_app\\todo.txt",
+#                 "r",
+#                 encoding="utf-8",
+#             )
+#             todos = file.readlines()
+#             file.close()
+#             todos.append(todo)
+#             file = open(
+#                 "F:\\aditya_git\\20_apps_45_days\\To_do_list_app\\todo.txt",
+#                 "w",
+#                 encoding="utf-8",
+#             )
+#             file.writelines(todos)
+#             print("Todo added. Next...\n")
+
+#             print("Next...", "\n")
+#             file.close()
+#         case "show":
+#             for index, element in enumerate(todos):
+#                 print(f"{index+1}) {element}", end="\n")
+#             print("\n")
+#             file = open(
+#                 "F:\\aditya_git\\20_apps_45_days\\To_do_list_app\\todo.txt",
+#                 "r",
+#                 encoding="utf-8",
+#             )
+#             todos = file.readlines()
+#             file.close()
+#         case "edit":
+#             number = int(
+#                 input("enter the index number of the element you want to change")
+#             )
+#             number = number - 1
+#             new_todo = input("enter the new todo")
+#             todos[number] = new_todo
+#         case "complete":
+#             number = int(input("enter the index number you want to remove "))
+#             todos.pop(number - 1)
+#         case "exit":
+
+#             break
+#         case _:
+#             print(
+#                 """enter only these options:
+#                   1) add
+#                   2) show
+#                   3) exit """
+#             )
+
+
 # -------------------------------------------------------------------------------#
-# Day 6
+# Day 7
 # -------------------------------------------------------------------------------#
 
 
