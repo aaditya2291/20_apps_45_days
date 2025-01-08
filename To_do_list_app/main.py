63956c248181d77dc7d52b7de89c61a5e30c834c
aaditya2291 <ayyala.aditya.2291@gmail.com>
2025-01-08 14:33:12 +0530

    commit

--- To_do_list_app/main.py
+++ To_do_list_app/main.py
@@ -202,6 +202,69 @@
 # -------------------------------------------------------------------------------#
 
 
+# while True:
+
+#     user_action = input("type add or show or edit or complete or exit ")
+#     user_action = user_action.strip()
+#     match user_action:
+#         case "add":
+#             todo = input("enter todo: ") + "\n"
+#             file = open(
+#                 "F:\\aditya_git\\20_apps_45_days\\To_do_list_app\\todo.txt",
+#                 "r",
+#                 encoding="utf-8",
+#             )
+#             todos = file.readlines()
+#             file.close()
+#             todos.append(todo)
+#             file = open(
+#                 "F:\\aditya_git\\20_apps_45_days\\To_do_list_app\\todo.txt",
+#                 "w",
+#                 encoding="utf-8",
+#             )
+#             file.writelines(todos)
+#             print("Todo added. Next...\n")
+
+#             print("Next...", "\n")
+#             file.close()
+#         case "show":
+#             for index, element in enumerate(todos):
+#                 print(f"{index+1}) {element}", end="\n")
+#             print("\n")
+#             file = open(
+#                 "F:\\aditya_git\\20_apps_45_days\\To_do_list_app\\todo.txt",
+#                 "r",
+#                 encoding="utf-8",
+#             )
+#             todos = file.readlines()
+#             file.close()
+#         case "edit":
+#             number = int(
+#                 input("enter the index number of the element you want to change")
+#             )
+#             number = number - 1
+#             new_todo = input("enter the new todo")
+#             todos[number] = new_todo
+#         case "complete":
+#             number = int(input("enter the index number you want to remove "))
+#             todos.pop(number - 1)
+#         case "exit":
+
+#             break
+#         case _:
+#             print(
+#                 """enter only these options:
+#                   1) add
+#                   2) show
+#                   3) exit """
+#             )
+
+
+# -------------------------------------------------------------------------------#
+# Day 8
+# -------------------------------------------------------------------------------#
+
+
 while True:
 
     user_action = input("type add or show or edit or complete or exit ")
@@ -209,35 +272,32 @@ while True:
     match user_action:
         case "add":
             todo = input("enter todo: ") + "\n"
-            file = open(
+            with open(
                 "F:\\aditya_git\\20_apps_45_days\\To_do_list_app\\todo.txt",
                 "r",
                 encoding="utf-8",
-            )
-            todos = file.readlines()
-            file.close()
+            ) as file:
+                todos = file.readlines()
             todos.append(todo)
-            file = open(
+            with open(
                 "F:\\aditya_git\\20_apps_45_days\\To_do_list_app\\todo.txt",
                 "w",
                 encoding="utf-8",
-            )
-            file.writelines(todos)
-            print("Todo added. Next...\n")
+            ) as file:
 
-            print("Next...", "\n")
-            file.close()
+                file.writelines(todos)
+                print("Todo added. Next...\n")
+                print("Next...", "\n")
         case "show":
             for index, element in enumerate(todos):
                 print(f"{index+1}) {element}", end="\n")
             print("\n")
-            file = open(
+            with open(
                 "F:\\aditya_git\\20_apps_45_days\\To_do_list_app\\todo.txt",
                 "r",
                 encoding="utf-8",
-            )
-            todos = file.readlines()
-            file.close()
+            ) as file:
+                todos = file.readlines()
         case "edit":
             number = int(
                 input("enter the index number of the element you want to change")
@@ -246,10 +306,12 @@ while True:
             new_todo = input("enter the new todo")
             todos[number] = new_todo
         case "complete":
-            number = int(input("enter the index number you want to remove "))
-            todos.pop(number - 1)
+            try:
+                number = int(input("enter the index number you want to remove "))
+                todos.pop(number - 1)
+            except NameError as ex:
+                print(ex)
         case "exit":
-
             break
         case _:
             print(
